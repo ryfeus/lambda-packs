50f4a48c4777850e53c2eb77e06a345ea56aa21b
ryfeus <ryfeus@gmail.com>
2019-01-30 13:09:53 -0800

    Added opencv_pillow 36

---
+++ Opencv_pil/source36/numpy/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+#
+# To compare versions robustly, use `numpy.lib.NumpyVersion`
+short_version = '1.16.0'
+version = '1.16.0'
+full_version = '1.16.0'
+git_revision = '971e2e89d08deeae0139d3011d15646fdac13c92'
+release = True
+
+if not release:
+    version = full_version
