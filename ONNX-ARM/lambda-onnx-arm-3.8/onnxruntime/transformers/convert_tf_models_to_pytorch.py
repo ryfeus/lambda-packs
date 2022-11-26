5dd99af6403a86b1f68c652c707c94d3b821cfc7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 04:03:16 +0000

    Adding tarfile member sanitization to extractall()

--- ONNX-ARM/lambda-onnx-arm-3.8/onnxruntime/transformers/convert_tf_models_to_pytorch.py
+++ ONNX-ARM/lambda-onnx-arm-3.8/onnxruntime/transformers/convert_tf_models_to_pytorch.py
@@ -73,7 +73,26 @@ def download_tf_checkpoint(model_name, tf_models_dir="tf_models"):
         # untar file
         import tarfile
         with tarfile.open(tar_dir, 'r') as tar_ref:
-            tar_ref.extractall(ckpt_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_ref, ckpt_dir)
             os.remove(tar_dir)
 
         return get_ckpt_prefix_path(ckpt_dir)
